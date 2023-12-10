de8b1fcdd0bbf5a04f2646bfae3784d407666eb1
Philipp Kolbe <philipp.kolbe1902@gmail.com>
2023-12-10 22:02:51 +0100

    initial commit

---
+++ unit-test-app/src/test/java/com/feedback2code/unittestapp/AppTest.java
@@ -0,0 +1,20 @@
+package com.feedback2code.unittestapp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
