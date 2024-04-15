d25f2cc99787011c955f7714563a58cf01debf2c
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2024-04-15 16:02:20 +0200

    feat: initialized MelonMan. Melon Man does not have a hat :(

---
+++ src/main/java/com/feedback2code/unittestapp/MelonMan.java
@@ -0,0 +1,14 @@
+package com.feedback2code.unittestapp;
+
+/**
+ * This class represents a MelonMan.
+ * A MelonMan can either wear a Melon or not wear a Melon.
+ */
+public class MelonMan 
+{
+    
+    public boolean hasHat()
+    {
+        return false;
+    }
+}
