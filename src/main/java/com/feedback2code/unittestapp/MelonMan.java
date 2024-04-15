32818326cfc2bb81cd6d37ffeb1d36bdec9e8c92
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2024-04-15 17:55:17 +0200

    feat: the Melon Man can't find his hat

--- src/main/java/com/feedback2code/unittestapp/MelonMan.java
+++ src/main/java/com/feedback2code/unittestapp/MelonMan.java
@@ -1,14 +1,27 @@
 package com.feedback2code.unittestapp;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * This class represents a MelonMan.
  * A MelonMan can either wear a Melon or not wear a Melon.
+ * Depends on if he finds his melon or not...
  */
 public class MelonMan 
 {
-    
+    private boolean hasMelonHat = false;
+
+    public void findHat(String directory) {
+        try {
+            hasMelonHat = Files.exists(Paths.get(directory, "melon.txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public boolean hasHat()
     {
-        return false;
+        return hasMelonHat;
     }
 }
